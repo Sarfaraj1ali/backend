1d5ae28c98c609b3358c104a9007b79418678a6e
Sarfaraj1ali <sksarfarajali2002@gmail.com>
2023-02-21 02:13:58 +0530

    commit3

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,17 @@ const { User} = require("../models/user");
 const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => { 
-	try {
+	// try {
 		console.log("HELOO")
 		// const { error } = validate(req.body);
 		// if (error)
 		// 	return res.status(400).send({ message: error.details[0].message });
 		await new User({ ...req.body}).save();
 		res.status(201).send({ message: "User created successfully" });
-	} catch (error) {
-		await new User({ ...req.body}).save();
-		res.status(200).send({ message: "User created" });
-	}
+	// } catch (error) {
+	// 	await new User({ ...req.body}).save();
+	// 	res.status(200).send({ message: "User created" });
+	// }
 });
 
 module.exports = router;
