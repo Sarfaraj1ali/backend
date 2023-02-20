efc257dc198c6d333d8af746121c5005d604c336
Sarfaraj1ali <sksarfarajali2002@gmail.com>
2023-02-21 01:51:26 +0530

    commit2

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ router.post("/", async (req, res) => {
 	try {
 		console.log("HELOO")
 		// const { error } = validate(req.body);
-		if (error)
-			return res.status(400).send({ message: error.details[0].message });
+		// if (error)
+		// 	return res.status(400).send({ message: error.details[0].message });
 		await new User({ ...req.body}).save();
 		res.status(201).send({ message: "User created successfully" });
 	} catch (error) {
