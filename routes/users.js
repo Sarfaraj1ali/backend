b88f5e1534bd92385773be7bde197a3b28abdc17
Sarfaraj1ali <sksarfarajali2002@gmail.com>
2023-02-22 09:12:15 +0530

    last

--- routes/users.js
+++ routes/users.js
@@ -3,21 +3,15 @@ const { User} = require("../models/user");
 const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => { 
-	const [name,last] = req.body;
-	const user = new User({
-		name,
-		last
-	})
 	try {
-		console.log("HELOO")
+		console.log(req.body)
 		// const { error } = validate(req.body);
 		// if (error)
 		// 	return res.status(400).send({ message: error.details[0].message });
-		
-		await user.save();
+		await new User({ ...req.body}).save();
 		res.status(201).send({ message: "User created successfully" });
 	} catch (error) {
-		res.status(500).send({error:error.message+"failed"});
+		res.status(500).send({ message: "enternal Server Error" });
 	}
 });
 
